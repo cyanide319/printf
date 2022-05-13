09b0fe5d941b7a0f2bc033686f04e61f2ed7a819
tbeaudoin <tristan-b@live.ca>
2022-05-13 16:19:14 -0400

    .h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,19 @@
 /*   By: tbeaudoi <tbeaudoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 16:11:23 by tbeaudoi          #+#    #+#             */
-/*   Updated: 2022/05/13 16:11:24 by tbeaudoi         ###   ########.fr       */
+/*   Updated: 2022/05/13 16:18:54 by tbeaudoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <unistd.h>
+
+# define B_10 "0123456789"
+# define B_16 "0123456789abcdef"
+# define B_16X "0123456789ABCDEF"
+
+int	ft_printf(const char *fmt, ...);
+
+#endif
