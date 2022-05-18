ede87e8f68a11e8b96370ece6052b2eab602d6f9
cyanide319 <tristan-b@live.ca>
2022-05-18 14:23:49 -0400

    printfp

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tbeaudoin <tbeaudoin@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 16:11:18 by tbeaudoi          #+#    #+#             */
-/*   Updated: 2022/05/16 16:18:04 by tbeaudoin        ###   ########.fr       */
+/*   Updated: 2022/05/18 14:07:49 by tbeaudoin        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,13 @@ int ft_putchar(int c)
     return (1);
 }
 
+int ft_putstr(char *str)
+{
+    while(*str)
+        write(1, str++, 1);
+    return (1);
+}
+
 int check_flags(va_list flag, const char frmt)
 {
     size_t printlen;
@@ -25,6 +32,10 @@ int check_flags(va_list flag, const char frmt)
     printlen = 0;
     if (frmt == 'c')
         printlen += ft_putchar(va_arg(flag, int));
+    else if (frmt == 's')
+        printlen += ft_putstr(va_arg(flag, char *));
+//    else if (frmt == 'p')
+//        printlen += ;
     return (printlen);
 }
 
@@ -52,7 +63,12 @@ int	ft_printf(const char *format, ...)
     return (printlen);
 }
 
-int main()
+#include<stdio.h>
+
+int main(void)
 {
-    ft_printf("%c", 'h');
+    ft_printf("%c \n   %s", 'h', "bon matin");
+    printf("\n%c\n", 'h');
+    ft_printf("%s", "bon matin");
+    printf("\n%s\n", "bon matin");
 }
